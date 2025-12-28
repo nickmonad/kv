cc6f3bc82fd85865bc010476b485dff74dcc1fb3
nickmonad <nick@ngmiller.io>
2025-12-28 16:12:59 -0600

    cleanup

--- bug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-redis-cli SET one aaa && redis-cli GET one
-redis-cli SET two bbb && redis-cli GET two
-redis-cli GET one
-redis-cli GET two
-redis-cli GET two
-redis-cli SET one aaa && redis-cli GET one
